3e0c0e67650a54d9d3f687fd58ce6a0b52f709ed
a25antonrr <antonrubinan10@gmail.com>
2026-03-09 11:17:20 +0100

    Ejercicio 9

--- src/main/kotlin/ejercicios/Ejercicio9.kt
+++ src/main/kotlin/ejercicios/Ejercicio9.kt
@@ -3,4 +3,11 @@ package ejercicios
 fun main(){
 //Desarrollar un programa que muestre la tabla de multiplicar del 5 (del 5
 //al 50)
+    val numero = 5
+    println("Tabla de multiplicar del $numero:")
+    for (i in 5..50) {
+        val resultado = numero * i
+        println("$numero x $i = $resultado")
+    }
+
 }
