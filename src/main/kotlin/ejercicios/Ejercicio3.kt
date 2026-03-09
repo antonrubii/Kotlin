9659204fc2a8fecba33097bbab278bb674dea072
a25antonrr <antonrubinan10@gmail.com>
2026-03-09 10:49:20 +0100

    Ejercicio 3

--- src/main/kotlin/ejercicios/Ejercicio3.kt
+++ src/main/kotlin/ejercicios/Ejercicio3.kt
@@ -3,4 +3,20 @@ package ejercicios
 fun main(){
 //Se ingresan tres notas de un alumno, si el promedio es mayor o igual a
 //cinco mostrar un mensaje "Apto".
+
+    println("Ingrese la primera nota:")
+    val nota1 = readLine()!!.toDouble()
+    println("Ingrese la segunda nota:")
+    val nota2 = readLine()!!.toDouble()
+    println("Ingrese la tercera nota:")
+    val nota3 = readLine()!!.toDouble()
+
+    val media = (nota1 + nota2 + nota3) / 3
+
+    if(media >= 5){
+        println("La media es: $media, el alumno es Apto")
+    }else{
+        println("La media es: $media, el alumno no es Apto")
+    }
+
 }
