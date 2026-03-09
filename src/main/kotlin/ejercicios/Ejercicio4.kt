9876dc887aa52b3ab16ab79e66b0645b1a29257a
a25antonrr <antonrubinan10@gmail.com>
2026-03-09 10:55:30 +0100

    Ejercicio 4

--- src/main/kotlin/ejercicios/Ejercicio4.kt
+++ src/main/kotlin/ejercicios/Ejercicio4.kt
@@ -4,4 +4,20 @@ fun main(){
 //Cargar un valor entero por teclado comprendido entre 1 y 99. Almacenar en
 //otra variable la cantidad de dígitos que tiene el valor ingresado por
 //teclado
+
+    println("Ingrese un numero entre 1 y 99:")
+    val numero = readLine()!!.toInt()
+    val digitos: Int
+    if (numero in 1..9){
+        digitos = 1
+        println("El numero tiene $digitos digito")
+    }else if (numero in 10..99){
+        digitos = 2
+        println("El numero tiene $digitos digitos")
+    }else {
+        println("El numero esta fuera del rango especificado (1-99)")
+    }
+
+
+
 }
