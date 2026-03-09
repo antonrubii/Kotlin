2d0bcb41952ccf2c08e3ba21673fccd9caaea72d
a25antonrr <antonrubinan10@gmail.com>
2026-03-09 11:04:27 +0100

    Ejercicio 7

--- src/main/kotlin/ejercicios/Ejercicio7.kt
+++ src/main/kotlin/ejercicios/Ejercicio7.kt
@@ -3,4 +3,10 @@ package ejercicios
 fun main(){
 //Mostrar los múltiplos de 8 hasta el valor 500. Debe aparecer en pantalla 8
 //- 16 - 24, etc.
+
+    for(i in 1..500){
+        if(i % 8 == 0){
+            println("$i -")
+        }
+    }
 }
